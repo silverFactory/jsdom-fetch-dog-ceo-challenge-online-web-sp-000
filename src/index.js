35e59c5178bc82792cc2c957b0e83d9d13b56862
Will Studabaker <will.studabaker@gmail.com>
2021-02-04 13:30:34 -0500

    commit tree 6b28f06b6bb560c1712c008de1e4709ff561a361

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function(){
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(json){
+    let images = json.message
+    for (const image of images){
+      let newImage = document.createElement('img')
+      newImage.src = image
+      document.getElementById('dog-image-container').appendChild(newImage)
+    }
+  })
+})
